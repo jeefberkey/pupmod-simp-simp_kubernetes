073a998db13752e18ef6b3186b5fa01094133861
Nick Miller <nick.miller@onyxpoint.com>
2018-01-23 17:29:59 -0500

    no more selinux

--- spec/acceptance/suites/default/00_internet_spec.rb
+++ spec/acceptance/suites/default/00_internet_spec.rb
@@ -6,8 +6,8 @@ test_name 'kubernetes using redhat provided packages'
 describe 'kubernetes using redhat provided packages' do
 
   masters    = hosts_with_role(hosts,'master')
-  controller = masters.first
   workers    = hosts_with_role(hosts,'worker')
+  controller = masters.first
 
   worker_manifest = <<-EOF
     include 'iptables'
@@ -28,9 +28,13 @@ describe 'kubernetes using redhat provided packages' do
       on(host, "sed -i 's/enforce_for_root//g' /etc/pam.d/*")
       on(host, 'echo "root:password" | chpasswd --crypt-method SHA256')
     end
-    it 'should disabe swap' do
+    it 'should disable swap' do
       on(host, 'swapoff -a')
       on(host, "sed -i '/swap/d' /etc/fstab")
+    end
+    it 'should disable selinux :(' do
+      on(host, 'setenforce 0')
+      on(host, "sed -i 's/enforcing/disabled/g' /etc/selinux/config /etc/selinux/config")
     end
     it 'should set up haveged' do
       host.install_package('epel-release')
@@ -44,7 +48,10 @@ describe 'kubernetes using redhat provided packages' do
           'docker',
           'KUBE-'
         ],
-        'simp_config::trusted_nets' => [
+        'iptables::ports' => {
+          '22' => nil
+        },
+        'simp_options::trusted_nets' => [
           '192.168.0.0/16',
           '10.0.0.0/8'
         ]
@@ -69,25 +76,31 @@ describe 'kubernetes using redhat provided packages' do
     end
   end
 
-  # hosts.each do |host|
-  #   on(host, 'systemctl enable kubelet')
-  # end
+  hosts.each do |host|
+    # on(host, 'systemctl enable kubelet')
+    it 'should not use cri networking' do
+      on(host, "sed -i '/network-plugin=cni/d' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf")
+      on(host, 'systemctl daemon-reload')
+      on(host, 'systemctl restart kubelet.service')
+    end
+  end
 
   it 'should use kubeadm to bootstrap cluster' do
-    require 'pry';binding.pry
+    # export KUBECONFIG=/etc/kubernetes/admin.conf
     # @token = 'aaaaaa-aaaabbbbccccdddd'
-    @controller_ip = controller.ip
     # init_log = on(controller, "kubeadm init --token #{@token} --pod-network-cidr=192.168.0.0/16")
-    init_log = on(controller, "kubeadm init --pod-network-cidr=192.168.55.0/24 --apiserver-advertise-address=#{@controller_ip}")
-    net_log  = on(controller, 'kubectl apply -f https://docs.projectcalico.org/v2.6/getting-started/kubernetes/installation/hosted/kubeadm/1.6/calico.yaml')
-    @token = init_log.grep(/kubeadm join/)
-    p net_log
+    controller_ip = fact_on(controller, 'ipaddress_eth1')
+    require 'pry';binding.pry
+    init_log = on(controller, "kubeadm init --pod-network-cidr=192.168.55.0/24 --apiserver-advertise-address=#{controller_ip}")
+    @join_cmd = init_log.grep(/kubeadm join/)
+
+    on(controller, 'kubectl apply -f https://docs.projectcalico.org/v2.6/getting-started/kubernetes/installation/hosted/kubeadm/1.6/calico.yaml')
   end
 
   workers.each do |host|
     it 'should connect to the master' do
-      on(host, @token)
-      # on(host, "kubeadm join --token #{@token}")
+      # on(host, "kubeadm join --token #{@join_cmd}")
+      on(host, @join_cmd)
     end
   end
 
